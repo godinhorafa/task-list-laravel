c6a68e964058deb83a341b275683f9a0340e6dea
Rafaella <rafaellagodinho@outlook.com>
2024-06-15 18:06:17 -0300

    layout for template inheritance

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 class Task
@@ -55,15 +56,24 @@ $tasks = [
   ),
 ];
 
-Route::get('/', function () use($tasks) {
+Route::get('/tasks', function () use($tasks) {
     return view('index', [
         'tasks' => $tasks
     ]);
 })->name('tasks.index');
 
-Route::get('/{id}', function($id) {
-    return 'One single task';
+Route::get('/tasks/{id}', function ($id) use ($tasks) {
+    $task = collect($tasks)->firstWhere('id', $id);
+
+    if(!$task) {
+      abort(Response::HTTP_NOT_FOUND);
+    }
+    return view('show', ['task' => $task]);
 })->name('tasks.show');
+
+Route::get('/', function() {
+    return redirect()->route(('tasks.index'));
+});
 
 //fallback define um retorno para rotas não existentes no projeto.
 Route::fallback(function () {
