e3a3232c8c2742eee189d5b53366a2dfb2923286
Rafaella <rafaellagodinho@outlook.com>
2024-06-16 15:01:00 -0300

    edit form and route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,20 @@ use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
 
 Route::get('/tasks', function () {
     return view('index', [
@@ -12,35 +26,67 @@ Route::get('/tasks', function () {
     ]);
 })->name('tasks.index');
 
-Route::view('/tasks/create', 'create')->name('tasks.create');
+Route::view('/tasks/create', 'create')
+    ->name('tasks.create');
+
+Route::get('/tasks/{id}/edit', function ($id) {
+    return view('edit', [
+        'task' => Task::findOrFail($id)
+    ]);
+})->name('tasks.edit');
 
 Route::get('/tasks/{id}', function ($id) {
-    return view('show', ['task' => Task::findOrFail($id)]);
+    return view('show', [
+        'task' => Task::findOrFail($id)
+    ]);
 })->name('tasks.show');
 
 Route::post('/tasks', function (Request $request) {
-  $data = $request->validate([
-    'title' => 'required|max:255',
-    'description' => 'required',
-    'long_description' => 'required'
-  ]);
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
 
-  $task = new Task;
-  $task->title = $data['title'];
-  $task->description = $data['description'];
-  $task->long_description = $data['long_description'];
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
 
-  $task->save();
-
-  return redirect()->route('tasks.show', ['id' => $task->id])
-  ->with('success','Task created successfully!');
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
-Route::get('/', function() {
-    return redirect()->route(('tasks.index'));
-});
+Route::put('/tasks/{id}', function ($id, Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
 
-//fallback define um retorno para rotas não existentes no projeto.
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
+
+// Route::get('/xxx', function () {
+//     return 'Hello';
+// })->name('hello');
+
+// Route::get('/hallo', function () {
+//     return redirect()->route('hello');
+// });
+
+// Route::get('/greet/{name}', function ($name) {
+//     return 'Hello ' . $name . '!';
+// });
+
 Route::fallback(function () {
     return 'Still got somewhere!';
 });
