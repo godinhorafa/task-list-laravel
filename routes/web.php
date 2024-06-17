32cf87995e65d225eba4e3c10a68780aa291a20f
Rafaella <rafaellagodinho@outlook.com>
2024-06-17 19:56:44 -0300

    delete button and route

--- routes/web.php
+++ routes/web.php
@@ -18,44 +18,49 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('tasks.index');
+  return redirect()->route('tasks.index');
 });
 
 Route::get('/tasks', function () {
-    return view('index', [
-        'tasks' => Task::latest()->get()
-    ]);
+  return view('index', [
+    'tasks' => Task::latest()->get()
+  ]);
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')
-    ->name('tasks.create');
+  ->name('tasks.create');
 
 Route::get('/tasks/{task}/edit', function (Task $task) {
-    return view('edit', [
-        'task' => $task
-    ]);
+  return view('edit', [
+    'task' => $task
+  ]);
 })->name('tasks.edit');
 
 Route::get('/tasks/{task}', function (Task $task) {
-    return view('show', [
-        'task' => $task
-    ]);
+  return view('show', [
+    'task' => $task
+  ]);
 })->name('tasks.show');
 
 Route::post('/tasks', function (TaskRequest $request) {
-    $task = Task::create($request->validated());
+  $task = Task::create($request->validated());
 
-    return redirect()->route('tasks.show', ['task' => $task->id])
-        ->with('success', 'Task created successfully!');
+  return redirect()->route('tasks.show', ['task' => $task->id])
+    ->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
-    $task->update($request->validated());
+  $task->update($request->validated());
 
-    return redirect()->route('tasks.show', ['task' => $task->id])
-        ->with('success', 'Task updated successfully!');
+  return redirect()->route('tasks.show', ['task' => $task->id])
+    ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
 
+Route::delete('/tasks/{task}', function (Task $task) {
+  $task->delete();
+  return redirect()->route('tasks.index')->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
+
 Route::fallback(function () {
-    return 'Still got somewhere!';
+  return 'Still got somewhere!';
 });
